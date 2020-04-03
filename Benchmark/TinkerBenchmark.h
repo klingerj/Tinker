d86e9ef4bf8a29f9b20699b9917e0480bf30d63c
Joseph Klinger <josephklinger@alumni.upenn.edu>
2020-04-03 17:44:21 -0400

    simpler benchmark code + change compile flags

--- Benchmark/TinkerBenchmark.h
+++ Benchmark/TinkerBenchmark.h
@@ -1,60 +1,47 @@
 #pragma once
 
-#include "../Source/Utils/Timer.h"
-
+#include <windows.h>
 #include <iostream>
 
 #define MAX(a, b) (a > b) ? a : b;
 #define MIN(a, b) (a < b) ? a : b;
 
-class Benchmark
-{
-public:
-    virtual void operator()() = 0;
-};
-
-template <typename T>
-class BenchmarkFuncContainer : Benchmark
-{
-private:
-    T t;
-
-public:
-    void operator()() override { t(); }
-};
-
-Timer g_Timer;
+#ifdef _WIN32
+#define OPTIMIZATIONS_ON __pragma(optimize( "", on ))
+#define OPTIMIZATIONS_OFF __pragma(optimize( "", off ))
+#endif
 
 #define NUM_SAMPLES 25
+#define SEC_2_MSEC 0.0001f
 
 #define TINKER_BENCHMARK_HEADER() \
-            std::cout << "Tinker Engine Benchmarks\n"; \
-            std::cout << "Number of times each benchmark will be run: " << NUM_SAMPLES << "\n\n";
+        std::cout << "Tinker Engine Benchmarks\n"; \
+        std::cout << "Number of times each benchmark will be run: " << NUM_SAMPLES << "\n\n";
 
 #define TINKER_PRINT_STATS(timeSamples) \
-            float bestTime = FLT_MAX, avgTime = 0.0f, worstTime = FLT_MIN; \
-            float timeSum = 0.0f; \
-            for (uint32_t i = 0; i < NUM_SAMPLES; ++i) \
-            { \
-                timeSum += timeSamples[i]; \
-                bestTime = MIN(bestTime, timeSamples[i]); \
-                worstTime = MAX(worstTime, timeSamples[i]); \
-            } \
-            avgTime = timeSum / NUM_SAMPLES; \
-            std::cout << "Best Time: "  << bestTime << " ms\n"; \
-            std::cout << "Avg Time: "   << avgTime <<  " ms\n"; \
-            std::cout << "Worst Time: " << worstTime <<  " ms\n\n";
+        float bestTime = FLT_MAX, avgTime = 0.0f, worstTime = FLT_MIN; \
+        float timeSum = 0.0f; \
+        for (uint32_t i = 0; i < NUM_SAMPLES; ++i) \
+        { \
+            timeSum += timeSamples[i]; \
+            bestTime = MIN(bestTime, timeSamples[i]); \
+            worstTime = MAX(worstTime, timeSamples[i]); \
+        } \
+        avgTime = timeSum / NUM_SAMPLES; \
+        std::cout << "Best Time: "  << bestTime << " ms\n"; \
+        std::cout << "Avg Time: "   << avgTime <<  " ms\n"; \
+        std::cout << "Worst Time: " << worstTime <<  " ms\n\n";
 
-#define TINKER_BENCHMARK(n, f) { \
-                std::cout << n << ":\n"; \
-                \
-                float timeSamples[NUM_SAMPLES] = {}; \
-                for (uint32_t i = 0; i < NUM_SAMPLES; ++i) \
-                { \
-                    g_Timer.Start(); \
-                    f(); \
-                    g_Timer.Stop(); \
-                    timeSamples[i] = g_Timer.ElapsedTime(); \
-                } \
-                TINKER_PRINT_STATS(timeSamples); \
-            }
+#define TINKER_BENCHMARK(str, func) { \
+        std::cout << str << ":\n"; \
+        float timeSamples[NUM_SAMPLES] = {}; \
+        LARGE_INTEGER start = {}, end = {}; \
+        for (uint32_t i = 0; i < NUM_SAMPLES; ++i) \
+        { \
+            QueryPerformanceCounter(&start); \
+            func(); \
+            QueryPerformanceCounter(&end); \
+            timeSamples[i] = (end.QuadPart - start.QuadPart) * SEC_2_MSEC; \
+        } \
+        TINKER_PRINT_STATS(timeSamples); \
+        }
