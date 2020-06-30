195afd0c34209152f117f9ce7f78e68a05cb84d3
Joseph Klinger <josephklinger@alumni.upenn.edu>
2020-06-30 14:23:34 -0400

    more vulkan resource creation

--- Include/Platform/Win32Vulkan.h
+++ Include/Platform/Win32Vulkan.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../../Include/Core/CoreDefines.h"
+
 #define VK_USE_PLATFORM_WIN32_KHR
 #include <vulkan/vulkan.h>
 
@@ -7,9 +9,15 @@
 
 typedef struct vulkan_context_res
 {
+    VkInstance instance = VK_NULL_HANDLE;
     VkDebugUtilsMessengerEXT debugMessenger;
-    VkSurfaceKHR surface;
-    VkInstance instance;
+    VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+    uint32 graphicsQueueIndex = 0xffffffff;
+    uint32 presentationQueueIndex = 0xffffffff;
+    VkDevice device = VK_NULL_HANDLE;
+    VkQueue graphicsQueue = VK_NULL_HANDLE;
+    VkQueue presentationQueue = VK_NULL_HANDLE;
+    VkSurfaceKHR surface = VK_NULL_HANDLE;
 } VulkanContextResources;
 
 int InitVulkan(VulkanContextResources* vulkanContextResources, HINSTANCE hInstance, HWND windowHandle);
