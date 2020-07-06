86044f7f4feb9eafc339a623f7d00ce85dbe40fe
Joseph Klinger <klingerj@seas.upenn.edu>
2020-07-06 16:37:38 -0400

    simple memory pool allocator

--- Include/Core/Allocators.h
+++ Include/Core/Allocators.h
@@ -6,7 +6,7 @@ namespace Tinker
 {
     namespace Memory
     {
-        template <size_t Size = 0, size_t Alignment = 1>
+        template <size_t Size = 0, uint32 Alignment = 1>
         class LinearAllocator
         {
         private:
@@ -34,11 +34,11 @@ namespace Tinker
                 if (m_ownedMemPtr) Platform::FreeAligned(m_ownedMemPtr);
             }
 
-            void Init(size_t size, size_t alignment)
+            void Init(size_t size, uint32 alignment)
             {
                 TINKER_ASSERT(size > 0);
                 m_size = size;
-                m_ownedMemPtr = (uint8*)Platform::AllocAligned(m_size, Alignment);
+                m_ownedMemPtr = (uint8*)Platform::AllocAligned(m_size, alignment);
             }
 
             uint8* Alloc(size_t size, size_t alignment)
@@ -65,6 +65,113 @@ namespace Tinker
             void Dealloc()
             {
                 m_nextAllocOffset = 0;
+            }
+        };
+
+        template <typename T>
+        struct pool_element
+        {
+            union
+            {
+                struct
+                {
+                    T m_data;
+                };
+                struct
+                {
+                    uint32 m_nextFreeEleIdx;
+                };
+            };
+        };
+
+        template <typename T, uint32 NumElements = 0, uint32 Alignment = 1>
+        class PoolAllocator
+        {
+        private:
+            uint32 m_numAllocdElements = 0;
+            uint32 m_maxPoolElements = 0;
+            uint32 m_freeListHead = 0;
+
+            template <typename U>
+            using PoolElement = struct pool_element<U>;
+            PoolElement<T>* m_pool = nullptr;
+            const uint32 m_elementSizeInBytes = sizeof(PoolElement<T>);
+
+            void InitFreeListPtrs()
+            {
+                for (uint32 uiEle = 0; uiEle < m_maxPoolElements - 1; ++uiEle)
+                {
+                    m_pool[uiEle].m_nextFreeEleIdx = uiEle + 1; // point to next element
+                }
+                m_pool[m_maxPoolElements - 1].m_nextFreeEleIdx = 0xffffffff;
+            }
+
+        public:
+            PoolAllocator()
+            {
+                TINKER_ASSERT(ISPOW2(Alignment));
+                m_maxPoolElements = NumElements;
+                if (m_maxPoolElements > 0)
+                {
+                    m_pool = (PoolElement<T>*)Platform::AllocAligned(m_maxPoolElements * m_elementSizeInBytes, Alignment);
+                    InitFreeListPtrs();
+                }
+                else
+                {
+                    // User must specify alloc'd memory with Init()
+                }
+            }
+
+            ~PoolAllocator()
+            {
+                if (m_pool) Platform::FreeAligned(m_pool);
+            }
+
+            inline T* PtrFromHandle(uint32 handle)
+            {
+                return &m_pool[handle].m_data;
+            }
+
+            void Init(uint32 maxPoolElements, size_t alignment)
+            {
+                TINKER_ASSERT(m_maxPoolElements == 0);
+                // Only call Init() if you did not provide the number of elements as a template at compile-time.
+
+                TINKER_ASSERT(maxPoolElements > 0 && maxPoolElements < 0xffffffff);
+                m_maxPoolElements = maxPoolElements;
+                m_pool = (PoolElement<T>*)Platform::AllocAligned(m_maxPoolElements * m_elementSizeInBytes, alignment);
+                InitFreeListPtrs();
+            }
+
+            uint32 Alloc()
+            {
+                if (m_freeListHead == 0xffffffff)
+                {
+                    // Pool is full - resize?
+                    TINKER_ASSERT(0);
+                    return 0xffffffff;
+                }
+                else
+                {
+                    uint32 newEle = m_freeListHead;
+                    m_freeListHead = m_pool[m_freeListHead].m_nextFreeEleIdx;
+                    ++m_numAllocdElements;
+
+                    if (m_numAllocdElements == m_maxPoolElements)
+                    {
+                        m_freeListHead = 0xffffffff;
+                    }
+
+                    return newEle;
+                }
+            }
+
+            void Dealloc(uint32 handle)
+            {
+                TINKER_ASSERT(m_numAllocdElements > 0);
+                TINKER_ASSERT(handle < m_maxPoolElements);
+                m_pool[handle].m_nextFreeEleIdx = m_freeListHead;
+                m_freeListHead = handle;
             }
         };
     }
