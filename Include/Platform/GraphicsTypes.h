3416ec424af1bd97bd958b6ada54ff558b03e029
Joseph Klinger <klingerj@seas.upenn.edu>
2020-07-07 17:49:46 -0400

    index buffer creation and drawing

---
+++ Include/Platform/GraphicsTypes.h
@@ -0,0 +1,23 @@
+#pragma once
+
+namespace Tinker
+{
+    namespace Platform
+    {
+        namespace Graphics
+        {
+            enum BufferType
+            {
+                eVertexBuffer = 0,
+                eIndexBuffer,
+                eBufferTypeMax
+            };
+
+            typedef struct staging_buffer_data
+            {
+                uint32 handle;
+                void* memory;
+            } StagingBufferData;
+        }
+    }
+}
