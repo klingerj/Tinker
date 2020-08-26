69c4d1f7e26660f20757ffc3901b85bea8956bcd
Joseph Klinger <josephklinger@alumni.upenn.edu>
2020-08-26 16:14:48 -0400

    got view and proj matrix working

--- Game/GameMain.cpp
+++ Game/GameMain.cpp
@@ -66,6 +66,9 @@ typedef struct virtual_camera_data
     v3f m_ref;
 } VirtualCamera;
 static VirtualCamera g_gameCamera = {};
+static float fovy = 0.785398f;
+static const float nearPlane = 0.1f;
+static const float farPlane = 1000.0f;
 static m4f g_projMat = m4f(1.0f);
 
 m4f CameraViewMatrix(const VirtualCamera* camera)
@@ -73,24 +76,32 @@ m4f CameraViewMatrix(const VirtualCamera* camera)
     v3f forward = camera->m_ref - camera->m_eye;
     Normalize(forward);
 
-    //v3f right = Cross(forward, WORLD_UP);
-    //v3f up = Cross(right, forward);
+    v3f right = Cross(forward, WORLD_UP);
+    Normalize(right);
+    v3f up = Cross(right, forward);
+    Normalize(up);
 
     m4f view;
-    //view[0] = v4f(right, 0.0f);
-    view[1] = v4f(forward, 0.0f);
-    //view[2] = v4f(up, 0.0f);
-    view[3] = v4f(camera->m_ref, 1.0f);
+    view[0] = v4f(right.x, up.x, forward.x, 0.0f);
+    view[1] = v4f(right.y, up.y, forward.y, 0.0f);
+    view[2] = v4f(right.z, up.z, forward.z, 0.0f);
+    view[3] = v4f(-Dot(camera->m_eye, right), -Dot(camera->m_eye, up), -Dot(camera->m_eye, forward), 1.0f);
     return view;
 }
 
 m4f PerspectiveProjectionMatrix()
 {
-    m4f projection;
+    m4f proj;
 
+    const float aspect = (float)currentWindowWidth / currentWindowHeight;
+    const float tanFov = tanf(fovy * 0.5f);
 
+    proj[0][0] = 1.0f / (aspect * tanFov); proj[1][0] = 0.0f;                     proj[2][0] = 0.0f;                                              proj[3][0] = 0.0f; // transform x to ndc
+    proj[0][1] = 0.0f;                     proj[1][1] = 1.0f / tanFov;            proj[2][1] = 0.0f;                                              proj[3][1] = 0.0f; // Z-up so y-coord gets normalized as depth
+    proj[0][2] = 0.0f;                     proj[1][2] = 0.0f;                     proj[2][2] = (-nearPlane - farPlane) / (nearPlane - farPlane);  proj[3][2] = (2.0f * farPlane * nearPlane) / (nearPlane - farPlane); // Z-up, so y-coord means depth
+    proj[0][3] = 0.0f;                     proj[1][3] = 0.0f;                     proj[2][3] = 1.0f;                                              proj[3][3] = 0.0f; // Z-up, so eye-space y-coord is used for perspective divide
 
-    return projection;
+    return proj;
 }
 
 // TODO: remove me
@@ -280,6 +291,9 @@ GAME_UPDATE(GameUpdate)
     {
         g_gameCamera.m_eye = v3f(3.0f, 3.0f, 3.0f);
         g_gameCamera.m_ref = v3f(0.0f, 0.0f, 0.0f);
+        currentWindowWidth = windowWidth;
+        currentWindowHeight = windowHeight;
+        g_projMat = PerspectiveProjectionMatrix();
 
         // Init network connection if multiplayer
         if (isMultiplayer)
