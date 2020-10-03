0e1297984820636732834750f6d395471bcd6cca
Joseph Klinger <josephklinger@alumni.upenn.edu>
2020-10-03 13:10:29 -0400

    tweak shader compile script

--- Scripts/compile_shaders_glsl2spv.bat
+++ Scripts/compile_shaders_glsl2spv.bat
@@ -2,13 +2,17 @@
 pushd ..\Shaders
 if NOT EXIST .\spv mkdir .\spv
 
+pushd .\spv
+del *.spv
+popd
+
 rem Vulkan
 set VulkanVersion=1.2.141.2
 echo Using Vulkan v%VulkanVersion%
 echo.
 set VulkanPath="C:\VulkanSDK"\%VulkanVersion%
 
-for %%i in (glsl\*.vert) do %VulkanPath%\Bin\glslangValidator.exe -V %%i -o .\spv\%%~ni_glsl_vert.spv
-for %%i in (glsl\*.frag) do %VulkanPath%\Bin\glslangValidator.exe -V %%i -o .\spv\%%~ni_glsl_frag.spv
+for %%i in (glsl\*.vert) do %VulkanPath%\Bin\glslangValidator.exe -V %%i -o .\spv\%%~ni_vert_glsl.spv
+for %%i in (glsl\*.frag) do %VulkanPath%\Bin\glslangValidator.exe -V %%i -o .\spv\%%~ni_frag_glsl.spv
 
 popd
