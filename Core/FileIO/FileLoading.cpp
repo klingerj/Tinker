a16aaa6386fb8b89a3d09287fce718dae932f7b0
Joseph Klinger <josephklinger@alumni.upenn.edu>
2020-10-07 18:11:05 -0400

    obj loading progress and some cleanup

--- Core/FileIO/FileLoading.cpp
+++ Core/FileIO/FileLoading.cpp
@@ -7,12 +7,60 @@ namespace Tinker
         namespace FileLoading
         {
             using namespace Math;
-            uint32 GetOBJVertCount(void* entireFileBuffer)
+
+            void scanLine(uint8* buffer, uint32* currentIndex)
             {
-                return 0;
+                startScan:
+                while (buffer[*currentIndex] != '\n' && buffer[*currentIndex] != '\r')
+                {
+                    ++*currentIndex;
+                }
+                ++*currentIndex; // move one past the newline
+
+                // Consume all consecutive '\n' and '\r' characters
+                if (buffer[*currentIndex] == '\n' || buffer[*currentIndex] == '\r')
+                {
+                    goto startScan;
+                }
             }
 
-            void ParseOBJ(v4f* dstPositionBuffer, v3f* dstNormalBuffer, v2f* dstUVBuffer, uint32* dstIndexBuffer, void* entireFileBuffer)
+            uint32 countVertLines(uint8* fileBuffer, uint32* currentIndex)
+            {
+                uint32 numVertices = 0;
+
+                while (fileBuffer[*currentIndex] == 'v' && fileBuffer[*currentIndex + 1] == ' ')
+                {
+                    scanLine(fileBuffer, currentIndex);
+                    ++numVertices;
+                }
+
+                return numVertices;
+            }
+
+            uint32 GetOBJVertCount(uint8* entireFileBuffer)
+            {
+                // Counter gets advanced as we scan lines of the files
+                uint32 currentIndex = 0;
+
+                // If first line starts with "v ", start reading lines
+                if (entireFileBuffer[currentIndex] == 'v' && entireFileBuffer[currentIndex + 1] == ' ')
+                {
+                    return countVertLines(entireFileBuffer, &currentIndex);
+                }
+                else
+                {
+                    // Skip lines until we find one that starts with "v "
+                    while (!(entireFileBuffer[currentIndex] == 'v' && entireFileBuffer[currentIndex + 1] == ' '))
+                    {
+                        scanLine(entireFileBuffer, &currentIndex);
+                    }
+
+                    // Start reading lines
+                    return countVertLines(entireFileBuffer, &currentIndex);
+                }
+            }
+
+            void ParseOBJ(v4f* dstPositionBuffer, v3f* dstNormalBuffer, v2f* dstUVBuffer, uint32* dstIndexBuffer, uint8* entireFileBuffer)
             {
                 while (0)
                 {
--- Include/Core/FileIO/FileLoading.h
+++ Include/Core/FileIO/FileLoading.h
@@ -8,15 +8,14 @@ namespace Tinker
     {
         namespace FileLoading
         {
-
             using namespace Math;
 
             // Returns the number of vertices in the obj file
             // Counts the number of rows that start with v
-            uint32 GetOBJVertCount(void* entireFileBuffer);
+            uint32 GetOBJVertCount(uint8* entireFileBuffer);
 
             // Parse the OBJ file and populate existing vertex attribute buffers
-            void ParseOBJ(v4f* dstPositionBuffer, v3f* dstNormalBuffer, v2f* dstUVBuffer, uint32* dstIndexBuffer, void* entireFileBuffer);
+            void ParseOBJ(v4f* dstPositionBuffer, v3f* dstNormalBuffer, v2f* dstUVBuffer, uint32* dstIndexBuffer, uint8* entireFileBuffer);
         }
     }
 }
