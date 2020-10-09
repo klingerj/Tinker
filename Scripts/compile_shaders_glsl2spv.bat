6ce9626b5c11091f07e38e78dd42f591f28d3bc3
Joseph Klinger <josephklinger@alumni.upenn.edu>
2020-10-09 14:27:37 -0400

    tweak/fix build script

--- Scripts/compile_shaders_glsl2spv.bat
+++ Scripts/compile_shaders_glsl2spv.bat
@@ -2,17 +2,25 @@
 pushd ..\Shaders
 if NOT EXIST .\spv mkdir .\spv
 
+echo.
+echo Deleting existing .spv files...
 pushd .\spv
 del *.spv
 popd
+echo Done.
 
 rem Vulkan
 set VulkanVersion=1.2.141.2
-echo Using Vulkan v%VulkanVersion%
-echo.
 set VulkanPath="C:\VulkanSDK"\%VulkanVersion%
+
+echo.
+echo Compiling GLSL shaders to SPV, using Vulkan SDK v%VulkanVersion% glslangValidator
+echo.
 
 for %%i in (glsl\*.vert) do %VulkanPath%\Bin\glslangValidator.exe -V %%i -o .\spv\%%~ni_vert_glsl.spv
 for %%i in (glsl\*.frag) do %VulkanPath%\Bin\glslangValidator.exe -V %%i -o .\spv\%%~ni_frag_glsl.spv
+
+echo.
+echo Done.
 
 popd
