00ce896ea80fc35a323dd6ec2e028b85ab885366
Joseph Klinger <klingerj@seas.upenn.edu>
2021-01-31 22:06:04 -0500

    gpu mem allocator skeleton

---
+++ Platform/Graphics/VulkanGPUMemAllocator.cpp
@@ -0,0 +1,44 @@
+#include "../../Include/Platform/Graphics/GPUMemAllocator.h"
+
+#include <vulkan/vulkan.h>
+
+namespace Tinker
+{
+namespace Platform
+{
+namespace Graphics
+{
+
+// Passthrough allocator
+const GPUMemAllocRecord& GPU_PassthroughAllocator::Alloc(uint32 size, uint32 resourceType)
+{
+    GPUMemAllocRecord& record = m_allocs[m_numTotalAllocs++];
+    record.size = size;
+    record.offset = 0; // TODO: get this?
+
+    VkMemoryAllocateInfo memAllocInfo = {};
+    memAllocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+    memAllocInfo.allocationSize = size;
+    memAllocInfo.memoryTypeIndex = 0 /*memTypeIndex*/; // TODO: get this?
+
+    VkResult result = VK_SUCCESS;// vkAllocateMemory(device, &allocInfo, nullptr, deviceMemory);
+    if (result != VK_SUCCESS)
+    {
+        Core::Utility::LogMsg("Platform", "Failed to allocate gpu memory!", Core::Utility::eLogSeverityCritical);
+        TINKER_ASSERT(0);
+    }
+
+    return record;
+}
+
+void GPU_PassthroughAllocator::Dealloc(const GPUMemAllocRecord& record)
+{
+    --m_numTotalAllocs;
+
+    // Call free memory
+}
+
+// TODO: a real allocator
+}
+}
+}
