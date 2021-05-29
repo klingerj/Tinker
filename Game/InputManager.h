f5761ef968de2f57e8605ad2f3da4c17ce45ec7a
Joseph Klinger <josephklinger@alumni.upenn.edu>
2021-05-29 17:43:33 -0400

    finish keypress api and camera controls

--- Game/InputManager.h
+++ Game/InputManager.h
@@ -16,8 +16,6 @@ typedef KEYCODE_CALLBACK(keycode_callback_func);
 struct InputManager
 {
 private:
-    enum { eMaxCallbacksPerKey = 4 };
-
     typedef struct input_state
     {
         Tk::Platform::KeycodeState keyCodes[Tk::Platform::Keycode::eMax];
@@ -25,14 +23,13 @@ private:
     InputState m_currentInputState = {};
     InputState m_previousInputState = {};
     
-    keycode_callback_func* m_callbacks[Tk::Platform::Keycode::eMax][eMaxCallbacksPerKey] = {};
-    uint8 m_callbackCounts[Tk::Platform::Keycode::eMax] = {};
-    // TODO: differentiate between stuff that should get called back on initial downpress vs held down / repeated
-
-    void CallAllCallbacksForKeycode(uint32 keycode, const Tk::Platform::PlatformAPIFuncs* platformFuncs);
+    keycode_callback_func* m_callbacks_keyDown[Tk::Platform::Keycode::eMax] = {};
+    keycode_callback_func* m_callbacks_keyDownRepeat[Tk::Platform::Keycode::eMax] = {};
+    // TODO: key up callbacks
 
 public:
-    void RegisterKeycodeCallback(uint32 keycode, keycode_callback_func callback);
+    void BindKeycodeCallback_KeyDown(uint32 keycode, keycode_callback_func callback);
+    void BindKeycodeCallback_KeyDownRepeat(uint32 keycode, keycode_callback_func callback);
     void UpdateAndDoCallbacks(const Tk::Platform::InputStateDeltas* inputStateDeltas, const Tk::Platform::PlatformAPIFuncs* platformFuncs);
 };
 
