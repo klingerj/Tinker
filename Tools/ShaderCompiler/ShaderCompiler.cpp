843e42e471ae2750e206419449e1999b0d2f7309
klingerj <16977116+klingerj@users.noreply.github.com>
2022-08-31 23:27:03 -0400

    dxc instance creation works

--- Tools/ShaderCompiler/ShaderCompiler.cpp
+++ Tools/ShaderCompiler/ShaderCompiler.cpp
@@ -1,10 +1,12 @@
-#include <windows.h>
-#include "inc/dxcapi.h"
+#include <atlbase.h>
+//#include <windows.h>
+#include <inc/dxcapi.h>
 
 #include <stdio.h>
 
 #include "ShaderCompiler.h"
 #include "DataStructures/Vector.h"
+#include "Platform/PlatformGameAPI.h"
 
 uint32 CompileAllShadersDX()
 {
@@ -18,9 +20,56 @@ uint32 CompileAllShadersVK()
     vec.Reserve(16u);
     printf("Compiling all shaders VK!\n");
 
-    // TODO: set compile args, compile each shader
-    DxcBuffer sourceBuffer;
-    sourceBuffer.Size = 0;
+    Tk::Core::Vector<const char*> args;
+    args.PushBackRaw("-E");
+    args.PushBackRaw("main");
+
+    args.PushBackRaw("-T");
+    args.PushBackRaw("vs_6_7"); // TODO: VS, PS, CS
+
+    //args.PushBackRaw("-Qstrip_debug"); // keep this if we want debug shaders
+    args.PushBackRaw("-Qstrip_reflect");
+
+    args.PushBackRaw("DXC_ARG_WARNINGS_ARE_ERRORS");
+    args.PushBackRaw("DXC_ARG_DEBUG");
+    args.PushBackRaw("DXC_ARG_PACK_MATRIX_COLUMN_MAJOR");
+
+    // TODO: add vulkan args
+
+    // TODO: #defines
+
+    HRESULT result;
+
+    CComPtr<IDxcUtils> pUtils;
+    CComPtr<IDxcCompiler3> pCompiler;
+    result = DxcCreateInstance(CLSID_DxcUtils, IID_PPV_ARGS(&pUtils));
+    result = DxcCreateInstance(CLSID_DxcCompiler, IID_PPV_ARGS(&pCompiler));
+
+    CComPtr<IDxcIncludeHandler> pIncludeHandler;
+    pUtils->CreateDefaultIncludeHandler(&pIncludeHandler);
+
+    CComPtr<IDxcBlobEncoding> pSource = nullptr;
+    pUtils->LoadFile(L"myshader.hlsl", nullptr, &pSource);
+    DxcBuffer Source;
+    Source.Ptr = pSource->GetBufferPointer();
+    Source.Size = pSource->GetBufferSize();
+    Source.Encoding = DXC_CP_ACP;
+
+    CComPtr<IDxcResult> pResults;
+    pCompiler->Compile(
+        &Source,                // Source buffer.
+        (LPCWSTR*)args.Data(),                // Array of pointers to arguments.
+        args.Size(),      // Number of arguments.
+        pIncludeHandler,        // User-provided interface to handle #include directives (optional).
+        IID_PPV_ARGS(&pResults) // Compiler output status, buffer, and errors.
+    );
+
+    CComPtr<IDxcBlobUtf8> pErrors = nullptr;
+    pResults->GetOutput(DXC_OUT_ERRORS, IID_PPV_ARGS(&pErrors), nullptr);
+    // Note that d3dcompiler would return null if no errors or warnings are present.  
+    // IDxcCompiler3::Compile will always return an error buffer, but its length will be zero if there are no warnings or errors.
+    //if (pErrors != nullptr && pErrors->GetStringLength() != 0)
+        //wprintf(L"Warnings and Errors:\n%S\n", pErrors->GetStringPointer());
 
     return 0;
 }
