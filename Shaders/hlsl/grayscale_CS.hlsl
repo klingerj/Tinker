024ae35b874d85bf06b8980ebfee59a5c75ce8c0
Joseph Klinger <16977116+klingerj@users.noreply.github.com>
2023-04-01 14:25:47 -0400

    compute shader creation fix

--- Shaders/hlsl/grayscale_CS.hlsl
+++ Shaders/hlsl/grayscale_CS.hlsl
@@ -1,11 +1,11 @@
-[[vk::binding(0, 0)]] Texture2D<float3> SrcColorImage;
-[[vk::binding(0, 1)]] RWTexture2D<float3> DstImage;
+[[vk::binding(0, 0)]] Texture2D SrcColorImage;
+[[vk::binding(1, 0)]] RWTexture2D<float4> DstImage;
 
 [numthreads(32, 0, 0)]
 void main(uint3 DispatchThreadID : SV_DispatchThreadID)
 {
     uint2 Coord = DispatchThreadID.xy;
-    float3 Color = SrcColorImage[Coord];
+    float3 Color = SrcColorImage[Coord].rgb;
     float Grayscale = Color.r; // todo do the dot product
     DstImage[Coord].rgb = Grayscale.rrr;
 }
